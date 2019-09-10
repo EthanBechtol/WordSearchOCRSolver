c647d12718d415e3364ef784573c4ebc1ca95f9f
Ethan Bechtol <48114346+EthanBechtol@users.noreply.github.com>
2019-09-09 17:39:37 -0700

    Add annotations to code, fix prepare_dir function folder recognition
    Prepare_dir now correctly recognizes existing folders and responds accordingly.
    The previous implementation never returned true even if directory existed.

--- main.py
+++ main.py
@@ -6,25 +6,38 @@ from PIL import Image
 import argparse
 
 
+first_dir_pass = True
+safe_to_delete = False
 def prepare_dir(word):
-    if not os.path.exists(os.getcwd() + "/words)"):
-        try:
-            os.mkdir(os.getcwd() + "/words")
-        except Exception as e:
-            print("Something went wrong!", e)
+    """Create a directory if it doesn't already exist to store resources generated in this program"""
+    global first_dir_pass
+    global safe_to_delete
 
-    if not os.path.exists(os.getcwd() + f"/words/{word}"):
-        try:
-            os.mkdir(os.getcwd() + f"/words/{word}")
-            print("Made path at " + os.getcwd() + f"/words/{word}")
-        except Exception as e:
-            print("Something went wrong!", e)
+    try:
+        os.mkdir(os.path.join(os.getcwd(), "words"))
+    except FileExistsError:
+        if first_dir_pass:
+            print("CRITICAL ERROR:" + os.path.join(os.getcwd(), "words") +
+                  "already exists prior to program start and cannot safely delete upon completion.")
+            # raise FileExistsError
+            exit(-1)
     else:
-        print('Exists')
+        # The directory did not exist prior to starting the program, so it is safe to delete it
+        # once the program finishes.
+        first_dir_pass = False
+        safe_to_delete = True
+
+    try:
+        os.mkdir(os.path.join(os.getcwd(), "words", word))
+        print("Made path at " + os.getcwd() + f"/words/{word}")
+    except Exception as e:
+        print("Something went wrong!", e)
+
 
 
 def get_canvas_forward(word: str):
-    char_width = 45
+    """Create & return an image containing the given word on a blank canvas to be used for matching"""
+    char_width = 42
     char_spacing = 32
     font = cv.FONT_HERSHEY_DUPLEX
 
@@ -46,20 +59,27 @@ def get_canvas_backward(word: str):
 
 
 def prepare_word_images(words: dict):
+    """
+    Create images containing the given dict of word:orientation in their proper orientations to be used
+    to match them in the larger puzzle. Uses helper functions to generate the images for readability/modularity purposes
+    """
     # TODO account for different spacing for larger letters (A, H, G, etc) and smaller letters (I, J, etc)
+    # TODO add other orientations
     for word, orientation in words.items():
         # FIXME Save to specific dir, not just in cwd
         # Forward orientation
         if orientation == "forward":
             canvas = get_canvas_forward(word)
-            cv.imwrite('canvas.png', canvas)
+            print(os.path.join(os.getcwd(), "words", f'{word}.png'))
+            cv.imwrite(os.path.join(os.getcwd(), "words", word, f'{word}_{orientation}.png'), canvas)
 
         elif orientation == "backward":
             canvas = get_canvas_backward(word)
-            cv.imwrite('canvas.png', canvas)
+            cv.imwrite(os.path.join(os.getcwd(), "words", word, f'{word}_{orientation}.png'), canvas)
 
 
 def get_np_filled(input_data):
+    """Return a valid numpy array given an uneven 2D array by substituting blank spaces with zeros"""
     separated = []
     for line in input_data:
         separated.append(list(line))
@@ -74,6 +94,7 @@ def get_np_filled(input_data):
 
 
 def get_columns(puzzle: list):
+    """Return a list of lists containing the columns of the given puzzle"""
     arr = get_np_filled(puzzle)
     columns = []
     for column in range(arr.shape[1]):
@@ -83,7 +104,7 @@ def get_columns(puzzle: list):
 
 
 def search_for_word(puzzle: list, word: str, diags_backslash, diags_forwardslash):
-    # Horizontally
+    # Search for word in horizontal combinations
     target = word.upper()
     for line in puzzle:
         if target in line:
@@ -91,7 +112,7 @@ def search_for_word(puzzle: list, word: str, diags_backslash, diags_forwardslash
         elif target in line[::-1]:
             return "backward"
 
-    # Vertically
+    # Search for word in vertical combinations
     columns_separated = get_columns(puzzle)
     # TODO if last column, 0s could be in the middle of the word
     for column in columns_separated:
@@ -105,7 +126,7 @@ def search_for_word(puzzle: list, word: str, diags_backslash, diags_forwardslash
         elif target in column[::-1]:
             return "up"  # bottom to top
 
-    # Diagonals
+    # Search for word in diagonal combinations
     for line in diags_backslash:
         if target in line:
             return "backslash"  # top-left to bot-right
@@ -122,11 +143,12 @@ def search_for_word(puzzle: list, word: str, diags_backslash, diags_forwardslash
 
 
 def get_diagonals(puzzle: list):
+    """Return 2 lists containing every diagonal combination in the given puzzle for use in search functions"""
     arr = get_np_filled(puzzle)
     diags_forwardslash = [arr[::-1, :].diagonal(i).tolist() for i in range(-arr.shape[0]+1, arr.shape[1])]
     diags_backslash = [arr.diagonal(i).tolist() for i in range(arr.shape[1]-1, -arr.shape[0], -1)]
 
-    # print(arr)
+    print(arr)
     # print(diags_backslash)
     # print(diags_forwardslash)
 
@@ -160,6 +182,7 @@ def preprocess_puzzle(words: set):
     diagstrings_forwardslash = ["".join(diag) for diag in diags_forwardslash]
     # print(diagstrings_forwardslash)
 
+    # Search the puzzle for target words and return their found orientation (None if not found)
     word_orientations = {}
     for word in words:
         found_orientation = search_for_word(fixed_list, word, diagstrings_backslash, diagstrings_forwardslash)
@@ -171,7 +194,7 @@ def preprocess_puzzle(words: set):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", nargs=1, help="Specify the puzzle image path to solve.")
+    parser.add_argument("-file", nargs=1, help="Specify the puzzle image path to solve.")
     parser.add_argument("-out", nargs=1, help="Select a destination for the output.")
     parser.add_argument("-display", action='store_true')
 
